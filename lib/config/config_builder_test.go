43075958f0032e2c8c25730adf9ce06dade19643
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2025-08-24 19:25:55 -0300

    Adds ConfigBuilder Tests

---
+++ lib/config/config_builder_test.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+
+func Test_ConfigBuilder(t *testing.T) {
+    tests := []struct {
+        name     	string
+        builder    	*ConfigModelBuilder
+        expected 	*ConfigModel
+		wantErr  	bool
+    }{
+        {
+            name:     "valid config",
+            builder:  NewConfigBuilder().
+						WithDBPath("/tmp/test.db").
+						WithTrackedAuthor("Test User", []string{
+							"test@example.com",
+						}).
+						WithTargetRepo("repo1"),
+            expected: &ConfigModel{
+				Version: 	 	CurrentVersion,
+				DBPath:        	"/tmp/test.db",
+				TrackedAuthor: ConfigAuthorModel{
+					Name:  "Test User",
+					Emails: []string{"test@example.com"},
+				},
+				TargetRepo:   	"repo1",
+				TrackedRepos: 	[]string{},
+			},
+			wantErr: 	false,
+        },
+        {
+            name:    	"invalid config - missing target repo",
+            builder: 	NewConfigBuilder().
+							WithTrackedAuthor("Test User", []string{
+								"test@example.com",
+							}),
+            expected: 	nil,
+            wantErr: 	true,
+        },
+        {
+            name:    	"invalid config - missing tracked author",
+            builder: 	NewConfigBuilder().
+							WithTargetRepo("repo1"),
+            expected: 	nil,
+            wantErr: 	true,
+        },
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            actualModel, err := tt.builder.Build()
+			if (tt.wantErr && err == nil) || (!tt.wantErr && err != nil) {
+				t.Errorf("expected error: %v, got: %v", tt.wantErr, err)
+			}
+			if !tt.wantErr && !reflect.DeepEqual(actualModel, tt.expected) {
+				t.Errorf("expected %v, got %v", tt.expected, actualModel)
+			}
+        })
+    }
+}
--- lib/config/config_model.go
+++ lib/config/config_model.go
@@ -1,8 +1,5 @@
 package config
 
-import (
-	"github.com/HideyoshiNakazone/tracko/lib/internal_errors"
-)
 
 var CurrentVersion = "v1"
 var DefaultDBPath = "$HOME/.config/tracko.db"
@@ -20,79 +17,3 @@ type ConfigModel struct {
 	TrackedRepos  []string          `mapstructure:"tracked_repos"`
 }
 
-// ConfigModelBuilder is a builder for ConfigModel
-type ConfigModelBuilder struct {
-	config *ConfigModel
-}
-
-func NewConfigBuilder() *ConfigModelBuilder {
-	return &ConfigModelBuilder{
-		config: &ConfigModel{
-			Version:       CurrentVersion,
-			DBPath:        DefaultDBPath,
-			TrackedAuthor: ConfigAuthorModel{},
-			TargetRepo:    "",
-			TrackedRepos:  []string{},
-		},
-	}
-}
-
-func (c *ConfigModelBuilder) WithDBPath(dbPath string) *ConfigModelBuilder {
-	c.config.DBPath = dbPath
-	return c
-}
-
-func (c *ConfigModelBuilder) WithTrackedAuthor(name string, emails []string) *ConfigModelBuilder {
-	c.config.TrackedAuthor.Name = name
-	c.config.TrackedAuthor.Emails = emails
-	return c
-}
-
-func (c *ConfigModelBuilder) WithTrackedAuthorName(name string) *ConfigModelBuilder {
-	c.config.TrackedAuthor.Name = name
-	return c
-}
-
-func (c *ConfigModelBuilder) WithTrackedAuthorEmails(emails []string) *ConfigModelBuilder {
-	c.config.TrackedAuthor.Emails = emails
-	return c
-}
-
-func (c *ConfigModelBuilder) WithAppendTrackedAuthorEmail(email string) *ConfigModelBuilder {
-	c.config.TrackedAuthor.Emails = append(c.config.TrackedAuthor.Emails, email)
-	return c
-}
-
-func (c *ConfigModelBuilder) WithTargetRepo(repo string) *ConfigModelBuilder {
-	c.config.TargetRepo = repo
-	return c
-}
-
-func (c *ConfigModelBuilder) WithTrackedRepos(repos []string) *ConfigModelBuilder {
-	c.config.TrackedRepos = repos
-	return c
-}
-
-func (c *ConfigModelBuilder) Build() (*ConfigModel, error) {
-	if c.config.Version == "" {
-		return nil, internal_errors.ErrInvalidConfig
-	}
-
-	if c.config.DBPath == "" {
-		return nil, internal_errors.ErrInvalidConfig
-	}
-
-	if c.config.TrackedAuthor.Name == "" {
-		return nil, internal_errors.ErrInvalidConfig
-	}
-
-	if len(c.config.TrackedAuthor.Emails) == 0 {
-		return nil, internal_errors.ErrInvalidConfig
-	}
-
-	if c.config.TargetRepo == "" {
-		return nil, internal_errors.ErrInvalidConfig
-	}
-
-	return c.config, nil
-}
